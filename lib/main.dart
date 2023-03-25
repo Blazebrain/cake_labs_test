2bb4eca092a66090c3e5826ad3096d1615f8016f
Blazebrain <davidadegoke16@gmail.com>
2023-03-25 07:46:48 +0100

    chore: Registering dependencies in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+import 'data/local_storage_service.dart';
+import 'domain/pin_code_repository.dart';
+import 'presentation/menu_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final localStorageService = LocalStorageService();
+  await localStorageService.init();
+  final pinCodeRepository = PinCodeRepository(localStorageService);
+
+  runApp(MyApp(pinCodeRepository: pinCodeRepository));
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final PinCodeRepository pinCodeRepository;
+
+  const MyApp({super.key, required this.pinCodeRepository});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
+    return MultiProvider(
+      providers: [
+        Provider<PinCodeRepository>.value(value: pinCodeRepository),
+      ],
+      child: MaterialApp(
+        title: 'Flutter PIN Code Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+        home: const MenuScreen(),
       ),
     );
   }
